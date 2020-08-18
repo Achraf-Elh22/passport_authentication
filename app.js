577138c0dd3ce49592c9fae2692f8d4c0e2425ac
Achraf Elhamzaoui <achrfelh22@gmail.com>
2020-08-18 12:05:54 +0100

    connect and Config the app with db

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const mainRoute = require('./routes/index');
 const usersRoutes = require('./routes/users');
 
 const app = express();
+
+// DB
+const dbUrl = require('./config/key').mongoUrl;
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successfully 👍👍'))
+  .catch((err) => console.log(err));
 
 // EJS
 app.use(expressLayouts);
---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoUrl:
+    'mongodb+srv://achrafelh22:test1234@cluster0.it62b.mongodb.net/<dbname>?retryWrites=true&w=majority    ',
+};
