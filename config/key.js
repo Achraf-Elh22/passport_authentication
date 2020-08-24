fc80977d685ec8467bc83492f233f466f9532054
Achraf <achrafelh22@gmail.com>
2020-08-24 11:53:07 +0100

    mongodb key

--- config/key.js
+++ config/key.js
@@ -1,4 +1,3 @@
 module.exports = {
-  mongoUrl:
-    'mongodb+srv://achrafelh22:test1234@cluster0.it62b.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  mongoUrl: "/////////////////////////////////////////",
 };
