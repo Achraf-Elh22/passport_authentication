c2b85bcc7707c890ef4352aaac4f4830fa36450f
Achraf <achrafelh22@gmail.com>
2020-08-24 11:33:56 +0100

    Finishing building this mini app

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ensureAuthentication: function (req, res, next) {
+    if (req.isAuthenticated()) return next();
+    req.flash("error_msg", "Please login to view this ressource");
+    res.redirect("/users/login");
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const { ensureAuthentication } = require("../config/auth");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("Welcome");
+});
+
+router.get("/dashboard", ensureAuthentication, (req, res) => {
+  res.render("dashboard", {
+    name: req.user.name,
+  });
 });
 
 module.exports = router;
